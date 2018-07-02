74a7107f23a577b0488237b86e0f68e68569edfd
Dhanraj Acharya <dhanrajacharya44@gmail.com>
2018-07-02 15:35:11 +0530

    change API endpoint

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const config = {
-  API_ENDPOINT: "https://tracklist-alpha.herokuapp.com/"
+  API_ENDPOINT: "https://tracklist-alpha-go.herokuapp.com/"
+  // API_ENDPOINT: "http://localhost:4000"
 };
