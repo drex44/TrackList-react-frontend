5887bc78a779c9f2b1b21782929f1fbffee3a26f
Dhanraj Acharya <dhanrajacharya44@gmail.com>
2018-07-27 19:17:14 +0530

    change README

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  API_ENDPOINT: "https://tracklist-alpha-go.herokuapp.com/",
-  // API_ENDPOINT: "http://localhost:4000",
+  // API_ENDPOINT: "https://tracklist-alpha-go.herokuapp.com/",
+  API_ENDPOINT: "http://localhost:4000",
   GOOGLE_CLIENT_ID:
-    "755576176377-f7l0sfcelhkh1beo7rjk2ki5g03jrlmk.apps.googleusercontent.com"
+    "xxxxx-xxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com"
 };
